11d51e625328892592bc00e0ddbf1f17873adde5
Ibrohim2302 <153183099+Ibrohim2302@users.noreply.github.com>
2024-04-26 08:44:07 +0500

    .

--- Domain/Entities/Order.cs
+++ Domain/Entities/Order.cs
@@ -1,4 +1,4 @@
-﻿using Domain.Abstract;
+﻿using Domain;
 using Domain.Enum;
 using System;
 using System.Collections.Generic;
--- Domain/Entities/Table.cs
+++ Domain/Entities/Table.cs
@@ -1,4 +1,4 @@
-﻿using Domain.Abstract;
+﻿using Domain;
 using Domain.Enum;
 
 namespace Domain.Entities;
