9b37ac7393668d066e2615916ccabe18279df300
Ibrohim2302 <153183099+Ibrohim2302@users.noreply.github.com>
2024-04-26 09:13:36 +0500

    Revert "3"
    This reverts commit 3c30abdd4776888837119bc2a9f81d4adcd527af.

---
+++ Infrastructure/DataBases/EFContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain;
+using Infrastructure.Persistence.TablesConfiguration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.DataBases;
+
+public class EFContext : DbContext
+{
+    public EFContext(DbContextOptions<EFContext> options) : base(options)
+    {
+
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Ignore<EntityBase>();
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(TablesConfiguration.CustomerConfiguration).Assembly);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(TablesConfiguration.MenuItemConfiguration).Assembly);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(TablesConfiguration.OrderItemConfiguration).Assembly);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(TablesConfiguration.OrderItemConfiguration).Assembly);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(TablesConfiguration.PaymentConfiguration).Assembly);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(TablesConfiguration.ReservationConfiguration).Assembly);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(TablesConfiguration.TableConfiguration).Assembly);
+    }
+}
